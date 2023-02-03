9f870890bbf275f43acac7627d783b861ac4c161
promise <promiseosunkwo@gmail.com>
2023-02-03 11:33:11 +0100

    This game allows the computer to guess your random number

--- guess.py
+++ guess.py
@@ -1,15 +1,36 @@
 import random
 
-def guess(x):
-    random_number = random.randint(1, x)
-    guess_number = 0
-    while random_number != guess_number:
-        guess_number = int(input("Enter a number: " ))
-        if(guess_number > random_number):
-            print("Sorry too high, Try again!")
-        elif(guess_number < random_number):
-            print("Sorry too low, Try again!")
-    print("Yaaaah! you got the number! Well done")
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low,high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too low (L), too high (H), correct (C)').lower()
+        if feedback == 'h':
+           high = guess - 1
+        elif feedback == 'l':
+           high = guess + 1
+    print("Yeayyy! Computer has the correct answer")
+
+computer_guess(10000)
+
+# def guess(x):
+#     random_number = random.randint(1, x)
+#     guess_number = 0
+#     while random_number != guess_number:
+#         guess_number = int(input("Enter a number: " ))
+#         if(guess_number > random_number):
+#             print("Sorry too high, Try again!")
+#         elif(guess_number < random_number):
+#             print("Sorry too low, Try again!")
+#     print("Yaaaah! you got the number! Well done")
 
 
-guess(3000)
+# guess(3000)
+
+
